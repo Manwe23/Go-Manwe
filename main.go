bd2787f8510f95ea5b48adb258bd9520ab7eefac
Manwe23 <lordmanwe23@gmail.com>
2013-11-25 00:40:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -44,26 +44,29 @@ func (q *queue) push(item int) {
 var mutex bool = false
 var sleep = false
 
-func fib(n int) int {
-	if n < 2 {
-		return n
-	}
-
-	return fib(n-1) + fib(n-2)
-}
-
 func golibroda(c <-chan bool, q *queue) {
+
 	for {
-		if x, err := q.pop(); err != nil {
+		if x, err := q.pop(); err == nil {
 			fmt.Println("Golibroda obsługuje klienta nr:", x)
 			time.Sleep(5 * time.Second)
 			fmt.Println("Golibroda konczy prace.")
 		} else {
 			break
 		}
+		for {
+			if !mutex {
+				break
+			}
+			time.Sleep(1 * time.Second)
+		}
+		mutex = true
+		fmt.Println("Golibroda idzie po kolejnego klienta.")
+		time.Sleep(3 * time.Second)
+		mutex = false
 	}
 	sleep = true
-	fmt.Println("Golibroda idzie spać. Zzzz...")
+	fmt.Println("Brak klientow.Golibroda idzie spać. Zzzz...")
 	for {
 		if !sleep {
 			break
@@ -72,6 +75,7 @@ func golibroda(c <-chan bool, q *queue) {
 	}
 	fmt.Println("Golibroda budzi sie!")
 	golibroda(c, q)
+
 }
 
 func poczekalnia(c <-chan bool, q *queue) {
@@ -80,12 +84,21 @@ func poczekalnia(c <-chan bool, q *queue) {
 	var client_nr int = 0
 	for {
 		client_nr++
-		fmt.Println("Przyszedl klient nr:", client_nr)
-		fmt.Println("Nastepny klient za:", w8_time)
 		time.Sleep(time.Duration(w8_time) * time.Second)
+		fmt.Println("Przyszedl klient nr:", client_nr)
 		q.push(client_nr)
-		if sleep {
+
+		if q.size < 2 {
+			for {
+				if !mutex {
+					break
+				}
+				time.Sleep(1 * time.Second)
+			}
+			mutex = true
+			time.Sleep(2 * time.Second)
 			sleep = false
+			mutex = false
 		}
 		w8_time = w8_time_generator.Intn(10)
 	}
@@ -95,12 +108,11 @@ func main() {
 	fmt.Println("Witaj swiat!")
 
 	var i int
-	var q queue //= &queue{}
+	var q queue
 	q.size = 0
 	c := make(chan bool)
 	c2 := make(chan bool)
 	go golibroda(c, &q)
 	go poczekalnia(c2, &q)
 	fmt.Scanf("%d", &i)
-	//fmt.Println(fib(30))
 }
